2913782c1cd5ec35c76c8b5f262884e1b707f68b
andela-gukpere <godson.ukpere@andela.co>
2015-11-15 01:07:13 +0300

    chore(Test Coverage WIP): Add test coverage config

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,66 @@
 (() => {
   'use strict';
+  const ENV = process.env.NODE_ENV || 'development';
+  if (ENV === 'development') {
+    require('dotenv').load();
+  }
+
   let gulp = require('gulp'),
-    jshint = require('jshint'),
-    cover = require('gulp-coverage'),
+    jshint = require('gulp-jshint'),
+    reporter = require('gulp-codeclimate-reporter'),
+    istanbul = require('gulp-istanbul'),
     nodemon = require('gulp-nodemon'),
     mocha = require('gulp-mocha'),
     paths = {
-      serverTests: ['./tests/server/**/*.spec.js']
+      serverTests: ['./tests/server/**/*.spec.js'],
+      app: ['./server/**.js']
     };
 
-  gulp.task('test:bend', () => {
+
+  gulp.task('pre-test', () => {
+    return gulp.src(paths.app)
+      .pipe(istanbul())
+      .pipe(istanbul.hookRequire());
+  });
+
+  gulp.task('test:bend', ['pre-test'], () => {
     return gulp.src(paths.serverTests)
-      .pipe(cover.instrument({
-        pattern: ['server/app.js'],
-        debugDirectory: 'debug'
-      }))
       .pipe(mocha({
         reporter: 'spec'
       }))
       .once('error', (err) => {
         throw new Error(err);
       })
-      .pipe(cover.gather())
-      .pipe(cover.format())
+      .pipe(istanbul({
+        includeUntested: true
+      }))
+      .pipe(istanbul.writeReports({
+        dir: './coverage',
+        reporters: ['lcov'],
+        reportOpts: {
+          dir: './coverage'
+        }
+      }))
       .pipe(gulp.dest('reports'))
-      .once('end', () => {
-        process.exit(0);
-      });
+      .once('end', () => {});
   });
 
   gulp.task('lint', () => {
-    return gulp.src(['./index.js', +
+    return gulp.src(['./index.js',
         './server/**/*.js', './tests/**/*.js'
       ])
       .pipe(jshint())
       .pipe(jshint.reporter('default'));
+  });
+
+  gulp.task('codeclimate-reporter', ['test:bend'], function() {
+    return gulp.src('./coverage/lcov.info', {
+        read: false
+      })
+      .pipe(reporter({
+        token: process.env.CODECLIMATE_TOKEN,
+        verbose: true
+      }));
   });
 
   gulp.task('nodemon', () => {
@@ -49,6 +75,6 @@
       });
   });
 
-  gulp.task('test', ['test:bend']);
+  gulp.task('test', ['codeclimate-reporter']);
   gulp.task('default', ['nodemon']);
 })();
